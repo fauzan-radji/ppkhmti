6e26fea2392076b52e6e6832f13391640fe1d190
fzrsahi <fazrul.anugrah17@gmail.com>
2022-12-08 18:12:17 +0800

    fix dashboard

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>PPK ORMAWA</title>
+  <title>Longyam Minapadi</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
